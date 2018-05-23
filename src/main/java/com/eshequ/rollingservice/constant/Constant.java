a8aedf2c7db6cbb75abefdf9903dc3294cc8d0cc
davidhardson <davidhardson@hotmail.com>
2018-05-23 13:49:52 +0800

    reset

--- src/main/java/com/eshequ/rollingservice/constant/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.eshequ.rollingservice.constant;
-
-public class Constant {
-
-	public static final String LIQUIDATE_HUIFU = "huifu";
-	
-}
--- src/main/java/com/eshequ/rollingservice/mapper/SpServeBillPayTradeMapper.java
+++
@@ -1,8 +0,0 @@
-package com.eshequ.rollingservice.mapper;
-
-import com.eshequ.rollingservice.common.CommonMapper;
-import com.eshequ.rollingservice.model.SpServeBillPayTrade;
-
-public interface SpServeBillPayTradeMapper extends CommonMapper<SpServeBillPayTrade> {
-   
-}
